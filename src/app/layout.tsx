3f14f88a75cb609062cf30c42644d2b4b20e44e0
manish rohilla <mrohilla600@gmail.com>
2026-03-04 08:59:22 +0000

    add a colorful theme in background with rgby elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,15 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-background relative">
-        <div className="relative z-0">
+        {/* Colorful RGBY Background Elements */}
+        <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+          <div className="bg-orb w-[400px] h-[400px] bg-red-400 -top-20 -left-20 animate-drift" />
+          <div className="bg-orb w-[500px] h-[500px] bg-green-300 top-1/4 -right-20 animate-drift" style={{ animationDelay: '-5s' }} />
+          <div className="bg-orb w-[450px] h-[450px] bg-blue-400 bottom-1/4 -left-32 animate-drift" style={{ animationDelay: '-10s' }} />
+          <div className="bg-orb w-[600px] h-[600px] bg-yellow-300 -bottom-32 right-0 animate-drift" style={{ animationDelay: '-15s' }} />
+        </div>
+        
+        <div className="relative z-10">
           {children}
         </div>
       </body>
